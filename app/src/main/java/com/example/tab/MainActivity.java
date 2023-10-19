cf0d62a6850936027467ccea258f8cc6fac08d8f
takieddine12 <algeriahero22@gmail.com>
2023-10-20 00:12:42 +0100

    new changes

---
+++ app/src/main/java/com/example/tab/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Animation"));
+        tabLayout.addTab(tabLayout.newTab().setText("Background"));
+    }
+}
